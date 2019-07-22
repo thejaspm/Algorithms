cd44eec490b119d4f00783d6c1b46414cd580d19
Thejas <thejas.p.murthy@gmail.com>
2019-07-22 21:32:14 +0200

    calc

---
+++ Stack/calculator.py
@@ -0,0 +1,18 @@
+# 
+#         3.1- Write a calculator that takes an input of the form 3+7-2 and outputs the result. 
+#           Only + and - will be included in the expression.
+#           Sample input:
+#               3+7-2
+#           Sample output:
+#               8
+#         3.2- Support parentheses.
+#           Sample input:
+#               5+16-((9-6)-(4-2))
+#           Sample output:
+#               20
+#         3.3- Support variables that consist of a single letter.
+#           Sample input:
+#               variables = {"e": 8, "y": 7, "k": 5}
+#               expression = "(e+3)-k+2"
+#           Sample Output:
+#               8
